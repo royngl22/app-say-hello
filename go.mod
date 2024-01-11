3973359439b4215ff220fbe42910fe5a4d27db2d
roy natanael <roy.natanaelngl02@gmail.com>
2024-01-11 12:10:48 +0700

    1st commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/royngl22/app-say-hello
+
+go 1.21.5
+
+require github.com/royngl22/go-say-hello-module v1.5.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	go_say_hello "github.com/royngl22/go-say-hello-module"
+)
+
+func main() {
+	go_say_hello.SayHello()
+}
